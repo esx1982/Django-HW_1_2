9c434f3f2772d04fdc8c311f8bf798bb035a2f4b
ESX <evgeniy_h@list.ru>
2024-07-25 00:05:42 +1000

    first commit

---
+++ lesson_1_2/views.py
@@ -0,0 +1,77 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+}
+
+def omlet(request):
+    servings = request.GET.get("servings")
+    context = {
+        'recipe': {
+        }
+    }
+    if servings != None:
+        for k, v in DATA.items():
+            if k == "omlet":
+                for a, b in v.items():
+                    context["recipe"][a] = b * int(servings)
+        return render(request, "demo.html", context)
+    else:
+        for k, v in DATA.items():
+            if k == "omlet":
+                for a, b in v.items():
+                    context["recipe"][a] = b
+        return render(request, "demo.html", context)
+def pasta(request):
+    servings = request.GET.get("servings")
+    context = {
+        'recipe': {
+        }
+    }
+    if servings != None:
+        for k, v in DATA.items():
+            if k == "pasta":
+                for a, b in v.items():
+                    context["recipe"][a] = b * int(servings)
+        return render(request, "demo.html", context)
+    else:
+        for k, v in DATA.items():
+            if k == "pasta":
+                for a, b in v.items():
+                    context["recipe"][a] = b
+        return render(request, "demo.html", context)
+
+def buter(request):
+    servings = request.GET.get("servings")
+    context = {
+        'recipe': {
+        }
+    }
+    if servings != None:
+        for k, v in DATA.items():
+            if k == "buter":
+                for a, b in v.items():
+                    context["recipe"][a] = b * int(servings)
+        return render(request, "demo.html", context)
+    else:
+        for k, v in DATA.items():
+            if k == "buter":
+                for a, b in v.items():
+                    context["recipe"][a] = b
+        return render(request, "demo.html", context)
+    
